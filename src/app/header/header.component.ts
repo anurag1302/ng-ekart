e9ac29d4aff420c696be479bf69e28f0ec8473a5
Anurag <anurag3487@gmail.com>
2024-05-13 23:20:13 +0530

    ng-ekart comp - styles array

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-header',
     // template: '<h2>Header</h2>',
-    templateUrl:'./header.component.html'
+    templateUrl:'./header.component.html',
+    styles:['a{text-decoration:none;margin:10px}','.ekart-logo{color:red}']
 })
 export class HeaderComponent {
 
