973a01c471f62384cba8f7e26bbfaad7be06f2e2
Anurag <anurag3487@gmail.com>
2024-05-13 23:42:51 +0530

    header comp - styleUrl populated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,8 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     // template: '<h2>Header</h2>',
     templateUrl:'./header.component.html',
-    styles:['a{text-decoration:none;margin:10px}','.ekart-logo{color:red}']
+    // styles:['a{text-decoration:none;margin:10px}','.ekart-logo{color:red}'],
+    styleUrl:'./header.component.scss'
 })
 export class HeaderComponent {
 
