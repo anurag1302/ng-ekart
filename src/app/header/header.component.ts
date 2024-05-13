de0a113058b8943ee25c4ef0f9325067f3fe3735
Anurag <anurag3487@gmail.com>
2024-05-13 23:12:46 +0530

    ng-ekart header comp html - templateUrl

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    template: '<h2>Header</h2>'
+    // template: '<h2>Header</h2>',
+    templateUrl:'./header.component.html'
 })
 export class HeaderComponent {
 
